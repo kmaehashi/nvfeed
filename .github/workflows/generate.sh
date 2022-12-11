954dacf06314d431371b8ae386ac5f7f733f691b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2022-12-12 00:45:50 +0900

    remove deprecated feeds and add ubuntu2004

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -4,23 +4,12 @@ rm -rf _generate
 cp -a _skeleton _generate
 
 python3 nvfeed.py \
-    --name 'NVIDIA CUDA Packages for Ubuntu 18.04 (x86_64)' \
-    --url 'https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/' \
+    --name 'NVIDIA CUDA Packages for Ubuntu 20.04 (x86_64)' \
+    --url 'https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/' \
     --file-suffix '.deb' \
-    --output '_generate/cuda-ubuntu1804-x86_64.atom'
-python3 nvfeed.py \
-    --name 'NVIDIA CUDA Packages for Ubuntu 18.04 (x86_64) - Experimental' \
-    --url 'https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/' \
-    --file-suffix '.deb' \
-    --output '_generate/TEST-cuda-ubuntu1804-x86_64-force-updated.atom' \
-    --force-updated
+    --output '_generate/cuda-ubuntu2004-x86_64.atom'
 python3 nvfeed.py \
     --name 'NVIDIA CUDA Packages for RHEL 8 (x86_64)' \
     --url 'https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/' \
     --file-suffix '.rpm' \
     --output '_generate/cuda-rhel8-x86_64.atom'
-python3 nvfeed.py \
-    --name 'NVIDIA Machine Learning Packages for Ubuntu 18.04 (x86_64)' \
-    --url 'https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/' \
-    --file-suffix '.deb' \
-    --output '_generate/machine-learning-ubuntu1804-x86_64.atom'
--- README.md
+++ README.md
@@ -5,7 +5,5 @@
 Get the latest updates on NVIDIA repositories.
 
 * CUDA
-    * [Ubuntu 18.04](https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/): https://kmaehashi.github.io/nvfeed/cuda-ubuntu1804-x86_64.atom
+    * [Ubuntu 20.04](https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/): https://kmaehashi.github.io/nvfeed/cuda-ubuntu2004-x86_64.atom
     * [RHEL 8](https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/): https://kmaehashi.github.io/nvfeed/cuda-rhel8-x86_64.atom
-* Machine Learning
-    * [Ubuntu 18.04](https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/): https://kmaehashi.github.io/nvfeed/machine-learning-ubuntu1804-x86_64.atom
